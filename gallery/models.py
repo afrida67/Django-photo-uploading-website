676122e2d71c48c82b3ab430a4ceab1c47496c4f
Afrida Aesha <afrida660@gmail.com>
2018-12-03 20:22:10 +0600

    album table add

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Album(models.Model):
+    caption = models.CharField(max_length=500)
+    hashtag = models.CharField(max_length=100)
+    photo = models.FileField()
