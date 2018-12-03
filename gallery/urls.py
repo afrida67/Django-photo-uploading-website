f1574965c865c79e76a6447f8174f399553a8d98
Afrida Aesha <afrida660@gmail.com>
2018-12-04 02:40:36 +0600

    manually added images

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,11 +1,12 @@
-##list of urls for music section
+
 from django.urls import path
 from . import views
 
 app_name = 'gallery'
 
 urlpatterns = [
-    # /music/
+    #/gallery/
     path('', views.index, name='index'),
+    path('<album_id>',views.detail, name='detail'),
 ]
  
--- gallery/views.py
+++ gallery/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from . models import Album
 from django.http import HttpResponse
-
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h2>hi this works")
+    all_photos = Album.objects.all() 
+    return render(request, 'gallery/index.html', {'all_photos' : all_photos})
+
+def detail(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'gallery/detail.html', {'album' : album})
    
