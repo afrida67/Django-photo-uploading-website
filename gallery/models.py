c1aa2b7e0cf1141e4a22676a39af598fde305d03
Afrida Aesha <afrida660@gmail.com>
2018-12-04 20:42:28 +0600

    upload done

--- gallery/models.py
+++ gallery/models.py
@@ -1,12 +1,15 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 class Album(models.Model):
     caption = models.CharField(max_length=500)
     hashtag = models.CharField(max_length=100)
-    #photo = models.FileField()
-    photo = models.CharField(max_length=1000)
+    photo = models.FileField()
+   # photo = models.CharField(max_length=1000)
 
     def __str__(self):
         return self.caption + ' - ' + self.hashtag
+
+    def get_absolute_url(self):
+        return reverse('gallery:detail', kwargs={"pk": self.pk})
