6c8c8957a3ad9ee04ba38dbd5f752b1d2b7c6d3b
Afrida Aesha <afrida660@gmail.com>
2018-12-05 19:42:52 +0600

    login added

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 # Create your views here.
 def signup_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             # log the user in
             return redirect('gallery:index') #after sigining in go to this page app
 
     else:
         form = UserCreationForm()
     return render(request,'accounts/signup.html',{'form':form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            #loggef in
+            user = form.get_user()
+            login(request, user)
+            return redirect('gallery:index')  
+    else:
+        form = AuthenticationForm()
+    
+    return render(request,'accounts/login.html',{'form':form})
